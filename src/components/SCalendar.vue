d61b88aa34cb14eb31f5076d6342441d78c158e4
stratosphr <stratosphr@gmail.com>
2020-05-07 11:19:54 +0200

    First commit

---
+++ src/components/SCalendar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-sheet height="350">
+        <v-calendar
+            :weekdays="[1, 2, 3, 4, 5, 6]"
+            type="week"
+        >
+
+        </v-calendar>
+    </v-sheet>
+</template>
+
+<script>
+	export default {
+		name: 's-calendar'
+	}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue     from 'vue'
+import App     from './App.vue'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+const ignoreWarnMessage = 'The .native modifier for v-on is only valid on components but it was used on <div>.'
+Vue.config.warnHandler = function (msg, vm, trace) {
+	if (msg !== ignoreWarnMessage) {
+		console.error(msg, trace)
+	}
+}
 
 new Vue({
-  render: h => h(App),
+	vuetify,
+	render: h => h(App)
 }).$mount('#app')
