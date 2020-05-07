7c9117556eb331c7ce011d3cf2d3b9ec5ab1b64d
stratosphr <stratosphr@gmail.com>
2020-05-07 15:42:38 +0200

    Formatted date displayed in custom header

--- src/components/CustomCalendar.vue
+++ src/components/CustomCalendar.vue
@@ -1,17 +1,29 @@
 <template>
     <s-calendar>
         <template #day-header="{date}">
-            {{date}}
+            <div class="text-capitalize text-truncate caption px-1">
+                {{ formattedDate(date) }}
+            </div>
         </template>
     </s-calendar>
 </template>
 
 <script>
+	import moment    from 'moment'
 	import SCalendar from '@/components/SCalendar'
+
+	moment.locale('fr')
 
 	export default {
 		name: 'CustomCalendar',
-		components: {SCalendar}
+
+		components: {SCalendar},
+
+		methods: {
+			formattedDate(date) {
+				return moment(date).format('dddd\xa0-\xa0DD\xa0MMM\xa0YYYY')
+			}
+		}
 	}
 </script>
 
