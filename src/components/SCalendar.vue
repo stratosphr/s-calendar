b9f481451829cc083ddad5026f32431b2094e2c2
stratosphr <stratosphr@gmail.com>
2020-05-09 22:53:53 +0200

    Displayed events with correct geometry

--- src/components/SCalendar.vue
+++ src/components/SCalendar.vue
@@ -12,6 +12,7 @@
             :start="start.format('YYYY-MM-DD')"
             :weekdays="[1, 2, 3, 4, 5, 6]"
             event-color="transparent"
+            ref="calendar"
             type="week"
         >
             <template #day-label-header="props">
@@ -20,18 +21,22 @@
                     v-bind="props"
                 />
             </template>
-            <template #event="{event}">
+            <template #event="{day}">
                 <div
-                    :style="{position: 'relative'}"
-                    class="red"
+                    :class="`event ${event.color}`"
+                    :key="eventIndex"
+                    :style="{ position: 'absolute', left: 0, right: 0, ...geometry(event), overflow: 'hidden'}"
+                    @mousedown="onMouseDownEvent(event)"
+                    v-for="(event, eventIndex) in eventsOnDate(day.date)"
+                    v-if="$refs.calendar"
                 >
-                    <div>
-                        Hello
-                    </div>
-                    <div>
-                        World
-                    </div>
+
                 </div>
+            </template>
+            <template #interval="props">
+                <div
+                    :class="`interval fill-height transparent`"
+                />
             </template>
         </v-calendar>
     </v-sheet>
@@ -49,10 +54,47 @@
 
 		data: () => ({
 			start: moment().startOf('week'),
-			end: moment().endOf('week')
+			end: moment().endOf('week'),
+			dragging: {
+				status: false,
+				event: null
+			},
+			events: [
+				{
+					start: '2020-05-09 02:10',
+					end: '2020-05-09 03:12',
+					color: 'teal'
+				},
+				{
+					start: '2020-05-06 03:23',
+					end: '2020-05-06 04:47',
+					color: 'red'
+				},
+				{
+					start: '2020-05-07 10:22',
+					end: '2020-05-07 10:45',
+					color: 'blue-grey'
+				},
+				{
+					start: '2020-05-05 02:00',
+					end: '2020-05-05 03:30',
+					color: 'green'
+				},
+				{
+					start: '2020-05-05 03:30',
+					end: '2020-05-05 04:30',
+					color: 'orange'
+				},
+				{
+					start: '2020-05-05 01:30',
+					end: '2020-05-05 01:45',
+					color: 'info'
+				}
+			]
 		}),
 
 		mounted() {
+
 		},
 
 		computed: {
@@ -62,12 +104,44 @@
 					end: day.format('YYYY-MM-DD 24:00')
 				})))
 			}
+		},
+
+		methods: {
+			eventsOnDate(date) {
+				return this.events.filter(event => moment(event.start).format('YYYY-MM-DD') === date)
+			},
+			geometry(event) {
+				const top = this.$refs.calendar.timeToY(moment(event.start).format('HH:mm')) + 1
+				const height = this.$refs.calendar.timeToY(moment(event.end).format('HH:mm')) - top - 2
+				return {
+					top: `${top}px`,
+					height: `${height}px`
+				}
+			},
+			onMouseDownEvent(event) {
+				console.log(event)
+			}
 		}
 
 	}
 </script>
 
 <style>
+    /*noinspection CssUnusedSymbol*/
+    .event {
+        user-select: none;
+    }
+
+    /*noinspection CssUnusedSymbol*/
+    .v-calendar-daily__day {
+        margin-top: -1px;
+    }
+
+    /*noinspection CssUnusedSymbol*/
+    .v-calendar-daily__intervals-body {
+        margin-top: -1px;
+    }
+
     /*noinspection CssUnusedSymbol*/
     .v-calendar-daily_head-weekday {
         display: none;
